fd0cbdec00987f2d3a1200bdfd492a046e456398
Annie Vo <annievo.av@hotmail.com>
2019-04-02 01:05:55 -0500

    currentLocation enable

--- lib/screens/service_locator.dart
+++ lib/screens/service_locator.dart
@@ -3,17 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:EmergenSeek/screens/nav_menu.dart';
 import 'package:EmergenSeek/screens/sos_quick_button.dart';
 import 'package:EmergenSeek/screens/settings.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ServiceLocatorPage extends StatefulWidget {
   @override
-  _ServiceLocatorPageState createState() {
-    return new _ServiceLocatorPageState();
-  }
+  ServiceLocatorPageState createState() => ServiceLocatorPageState();
 }
 
-// The screen itself, contains an app bar, the map, and the SOS button
-class _ServiceLocatorPageState extends State<ServiceLocatorPage> {
+class ServiceLocatorPageState extends State<ServiceLocatorPage> {
+  // ?options current location
+  bool mapToggle = false;
+  var currentLocation;
+  // Google Map Controller for onMapCreated
+  GoogleMapController mapController;
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  void initState() {
+    super.initState();
+    Geolocator().getCurrentPosition().then((currLocation) {
+      setState(() {
+        currentLocation = currLocation;
+        mapToggle = true;
+      });
+    });
+  }
+
+  // The screen itself with appbar, googleMap, and floating SOS
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -35,36 +52,49 @@ class _ServiceLocatorPageState extends State<ServiceLocatorPage> {
           ],
         ),
         body: Stack(
+          // Stack over the full-screen
           children: <Widget>[
-            googleMap(context),
-            buildContainer(),
+            _googleMap(),
+            _buildContainer(),
+            //_makeMaker(),
           ],
         ),
         floatingActionButton: QuickSOS());
   }
-}
 
-// Goole Map Controller for zoom_in function
-Completer<GoogleMapController> _controller = Completer();
-Widget googleMap(BuildContext context) {
+  // The Google Map itself, using var currentlocation
+  Widget _googleMap() {
     return Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition:
-              // TO DO: Get_User_LatLng and place in 'target'
-              CameraPosition(target: LatLng(33.5779, -101.8552), zoom: 12),
-              //Define Google Map Controller with controller
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-          markers: {
-            // TO DO: SELECTED_MarkerID such as pharmacy, hospital
-            pharmacyMarker, hospitalMarker
-          },
-        ));
+        child: mapToggle
+            ? GoogleMap(
+                mapType: MapType.normal,
+                myLocationEnabled: true,
+                compassEnabled: true,
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(
+                        currentLocation.latitude, currentLocation.longitude),
+                    zoom: 12),
+                //Define Google Map Controller with controller
+                onMapCreated: (mapController) {
+                  _controller.complete(mapController);
+                })
+            : Center(
+                child: Text(
+                'Loading.. Please wait..',
+                style: TextStyle(fontSize: 20.0),
+              )));  
   }
+
+  Marker pharmacyMarker = Marker(
+    markerId: MarkerId('pharmacy'),
+    position: LatLng(33.5897, -101.8560),
+    infoWindow: InfoWindow(title: 'Pharmacy'),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueBlue,
+    ),
+  );
 
 // Navigating MARKER long/lat to relocate with BOXES && vs
   Future<void> goToLocation(double lat, double long) async {
@@ -77,132 +107,100 @@ Widget googleMap(BuildContext context) {
     )));
   }
 
-Marker pharmacyMarker = Marker(
-  markerId: MarkerId('pharmacy'),
-  // TO DO: Get_Searched_LatLng
-  position: LatLng(33.5897, -101.8560),
-  // TO DO: Get_SELECTED_Searched_Place
-  infoWindow: InfoWindow(title: 'Pharmacy'),
-  icon: BitmapDescriptor.defaultMarkerWithHue(
-    BitmapDescriptor.hueBlue,
-  ),
-);
+// The bottom full-boxes with ALIGN. and LISTVIEW, fitted-boxes
+  Widget _buildContainer() {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 150.0,
+          child: mapToggle
+              ? ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: boxes( //put info in here
+                          "https://i.ibb.co/z8CgfZy/453947-walgreens-photo.jpg",
+                          33.5897,
+                          -101.8560,
+                          "Pharmacy"),
+                    ),
+                  ],
+                )
+              : Center()),
+    );
+  }
 
-// TO DO: Loop for more Get_Searched_LatLng
-Marker hospitalMarker = Marker(
-  markerId: MarkerId('hospital'),
-  // TO DO: Get_Searched_LatLng
-  position: LatLng(33.5885, -101.8931),
-  // TO DO: Get_SELECTED_Searched_Place
-  infoWindow: InfoWindow(title: 'Hospital'),
-  icon: BitmapDescriptor.defaultMarkerWithHue(
-    BitmapDescriptor.hueBlue,
-  ),
-);
-
-// TO DO: API details
-Widget buildContainer() {
-  return Align(
-    alignment: Alignment.bottomLeft,
-    child: Container(
-      margin: EdgeInsets.symmetric(vertical: 20.0),
-      height: 150.0,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: <Widget>[
-          SizedBox(
-            width: 10.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            // TO DO: API details
-            child: boxes(
-                "https://i.ibb.co/z8CgfZy/453947-walgreens-photo.jpg",
-                33.5897, -101.8560,
-                "Pharmacy"),
-          ),
-          SizedBox(
-            width: 10.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            // TO DO: API details
-            child: boxes(
-                "https://i.ibb.co/zmW2gnf/2017-11-08large-data-breach.jpg",
-                33.5885, -101.8931,
-                "Hospital"),
-          )
-        ],
-      ),
-    ),
-  );
-}
-
-Widget boxes(String _image, double lat, double long, String locationName) {
-  return GestureDetector(
-    onTap: () {
-      goToLocation(lat, long);
-    },
-    child: Container(
-      child: new FittedBox(
-        child: Material(
-            color: Colors.white,
-            elevation: 14.0,
-            borderRadius: BorderRadius.circular(24.0),
-            shadowColor: Color(0x802196F3),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: 180,
-                  height: 200,
-                  child: ClipRRect(
-                    borderRadius: new BorderRadius.circular(24.0),
-                    child: Image(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(_image),
+  Widget boxes(String _image, double lat, double long, String locationName) {
+    return GestureDetector(
+      onTap: () {
+        goToLocation(lat, long);
+      },
+      child: Container(
+        child: new FittedBox(
+          child: Material(
+              color: Colors.white,
+              elevation: 14.0,
+              borderRadius: BorderRadius.circular(24.0),
+              shadowColor: Color(0x802196F3),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 180,
+                    height: 200,
+                    child: ClipRRect(
+                      borderRadius: new BorderRadius.circular(24.0),
+                      child: Image(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(_image),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: myDetailsContainer(locationName),
-                  ),
-                )
-              ],
-            )),
-      ),
-    ),
-  );
-}
-
-// TO DO: API details
-Widget myDetailsContainer(String locationName) {
-  return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-              child: Text(
-            locationName,
-            style: TextStyle(
-                color: Color(0xff6200ee),
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold),
-          )),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: myDetailsContainer(locationName),
+                    ),
+                  )
+                ],
+              )),
         ),
-        SizedBox(height: 5.0),
-        Container(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  child: Text("Details: ... ",
-                      style: TextStyle(color: Colors.black54, fontSize: 18.0)),
-                )
-              ]),
-        )
-      ]);
+      ),
+    );
+  }
+
+  Widget myDetailsContainer(String locationName) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                child: Text(
+              locationName,
+              style: TextStyle(
+                  color: Color(0xff6200ee),
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            )),
+          ),
+          SizedBox(height: 5.0),
+          Container(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    child: Text("Details: ... ",
+                        style:
+                            TextStyle(color: Colors.black54, fontSize: 18.0)),
+                  )
+                ]),
+          )
+        ]);
+  }
 }
